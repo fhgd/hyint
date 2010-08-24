1105a6ade08ea27091096a039887f08b2d8c009b
Friedrich Hagedorn <fhgd@users.noreply.github.com>
2010-08-24 18:34:10 +0200

    Simulationsbsp.:  Synchrone Entladung des Piezogenerators (f, g zeitabh.)

--- hybridsim.py
+++ hybridsim.py
@@ -9,7 +9,7 @@ def fsolve(g, x0, x1, eps):
     return x1
 
 def odeint(f, g, h, x0, t0, t1, dt, eps):
-    """Integration eines hybriden Systems (erste Ordnung)"""
+    """Integration eines hybriden Systems (erster Ordnung)"""
 
     # start values
     t = [t0]
@@ -18,13 +18,15 @@ def odeint(f, g, h, x0, t0, t1, dt, eps):
     while t[-1] < t1:
 
         # integrate
-        while g(x[-1]) >=0:
-            x.append(x[-1] + f(x[-1])*dt)
+        while g(t[-1], x[-1]) >= 0 and t[-1] < t1:
+            x.append(x[-1] + f(t[-1], x[-1]) * dt)
             t.append(t[-1] + dt)
+        if not t[-1] < t1:
+            break
 
         # correct the last step, which was too big
-        x_local = lambda t_: x[-2] + f(x[-2])*t_
-        g__x_local = lambda t_: g(x_local(t_))
+        x_local = lambda t_: x[-2] + f(t[-2] + t_, x[-2]) * t_
+        g__x_local = lambda t_: g(t[-2] + t_, x_local(t_))
         dt_ = fsolve(g__x_local, 0, dt, eps)
         x[-1] = x_local(dt_)
         t[-1] = t[-2] + dt_
@@ -38,13 +40,24 @@ def odeint(f, g, h, x0, t0, t1, dt, eps):
 
 if __name__ == '__main__':
 
-    def f(x):
-        return -x
+    R = 0.5
+    C = 1.0
 
-    def g(x):
-        return x - 1
+    import math
 
-    def h(x):
-        return 10.0
+    def IP(t):
+        return math.sin(t)
 
-    t, x = odeint(f, g, h, x0=10.0, t0=0.0, t1=7.0, dt=0.1, eps=1e-6)
+    def f(t, UC):
+        IC = IP(t) - UC/R
+        return IC/C
+
+    def g(t, UC):
+        IC = abs(IP(t)) - abs(UC)/R
+        return IC
+
+    def h(UC):
+        return 0.0
+
+    t, x = odeint(f, g, h,
+        x0=0.0, t0=0.0, t1=3*math.pi, dt=0.001, eps=1e-15)
