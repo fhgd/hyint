1c279262dc1975d3eb4311faf76a7d3352dabe87
Friedrich Hagedorn <fhgd@users.noreply.github.com>
2010-08-25 15:14:19 +0200

    Realisierung mit einer FSM (sogar etwas schneller)

--- hybridsim.py
+++ hybridsim.py
@@ -8,43 +8,45 @@ def fsolve(g, x0, x1, eps):
         x0, x1 = x1, x1 - g(x1) * (x1 - x0) / (g(x1) - g(x0))
     return x1
 
-def odeint(f, events, actions, x0, t0, t1, dt, eps):
+def odeint(f, x0, t0, t1, dt, graph, z0, eps):
     """Integration eines hybriden Systems (erster Ordnung)"""
 
     # start values
     t = [t0]
     x = [x0]
+    z = z0
 
     while t[-1] < t1:
+
         # integrate until at least one event is detected
         while 1:
-            # integrate with a euler step
+            # integrate with an euler step
             x.append(x[-1] + f(t[-1], x[-1]) * dt)
             t.append(t[-1] + dt)
             # test all event functions
-            idx = [i for i, ev in enumerate(events) if ev(t[-1], x[-1]) < 0]
-            if idx:
+            events = [ev for ev in graph[z].iterkeys() if ev(t[-1], x[-1]) < 0]
+            if events:
                 # at least one event is detected
                 break
 
         # find the first event
+        dt_min = dt
         x_local = lambda t_: x[-2] + f(t[-2] + t_, x[-2]) * t_
-        dt_all = []
-        for i in idx:
-            ev = events[i]
+        for ev in events:
             ev__x_local = lambda t_: ev(t[-2] + t_, x_local(t_))
-            dt_all.append(fsolve(ev__x_local, 0, dt, eps))
-        # the first event has the smallest dt
-        dt_, i = min((dt_, i) for dt_, i in zip(dt_all, idx))
+            dt_ = fsolve(ev__x_local, 0, dt, eps)
+            if dt_ <= dt_min:
+                dt_min = dt_
+                event = ev
 
-        # correct the last integration step, which was too big
-        x[-1] = x_local(dt_)
-        t[-1] = t[-2] + dt_
+        # correct the last integration step, which was too far
+        x[-1] = x_local(dt_min)
+        t[-1] = t[-2] + dt_min
 
-        # discrete step in the continious state x
-        x.append(actions[i](x[-1]))
+        # transition and action of the FSM
+        z = graph[z][event]
+        x.append(z(t[-1], x[-1]))
         t.append(t[-1])
-
     return t, x
 
 
@@ -66,8 +68,8 @@ if __name__ == '__main__':
         IC = abs(IP(t)) - abs(UC)/R
         return IC
 
-    def action(UC):
+    def action(t, UC):
         return 0.0
 
-    t, x = odeint(f, [event], [action],
-        x0=0.0, t0=0.0, t1=3*math.pi, dt=0.001, eps=1e-15)
+    graph = {action : {event : action}}
+    t, x = odeint(f, 0.0, 0.0, 3*math.pi, 0.001, graph, action, 1e-15)
