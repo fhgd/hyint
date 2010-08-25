cc96ae4869ea04f91c4089def46e26fe4c736f14
Friedrich Hagedorn <fhgd@users.noreply.github.com>
2010-08-25 11:59:39 +0200

    Fuer mehrere events, actions verallgemeinert

--- hybridsim.py
+++ hybridsim.py
@@ -8,7 +8,7 @@ def fsolve(g, x0, x1, eps):
         x0, x1 = x1, x1 - g(x1) * (x1 - x0) / (g(x1) - g(x0))
     return x1
 
-def odeint(f, g, h, x0, t0, t1, dt, eps):
+def odeint(f, events, actions, x0, t0, t1, dt, eps):
     """Integration eines hybriden Systems (erster Ordnung)"""
 
     # start values
@@ -16,23 +16,33 @@ def odeint(f, g, h, x0, t0, t1, dt, eps):
     x = [x0]
 
     while t[-1] < t1:
-
-        # integrate
-        while g(t[-1], x[-1]) >= 0 and t[-1] < t1:
+        # integrate until at least one event is detected
+        while 1:
+            # integrate with a euler step
             x.append(x[-1] + f(t[-1], x[-1]) * dt)
             t.append(t[-1] + dt)
-        if not t[-1] < t1:
-            break
+            # test all event functions
+            idx = [i for i, ev in enumerate(events) if ev(t[-1], x[-1]) < 0]
+            if idx:
+                # at least one event is detected
+                break
 
-        # correct the last step, which was too big
+        # find the first event
         x_local = lambda t_: x[-2] + f(t[-2] + t_, x[-2]) * t_
-        g__x_local = lambda t_: g(t[-2] + t_, x_local(t_))
-        dt_ = fsolve(g__x_local, 0, dt, eps)
+        dt_all = []
+        for i in idx:
+            ev = events[i]
+            ev__x_local = lambda t_: ev(t[-2] + t_, x_local(t_))
+            dt_all.append(fsolve(ev__x_local, 0, dt, eps))
+        # the first event has the smallest dt
+        dt_, i = min((dt_, i) for dt_, i in zip(dt_all, idx))
+
+        # correct the last integration step, which was too big
         x[-1] = x_local(dt_)
         t[-1] = t[-2] + dt_
 
         # discrete step in the continious state x
-        x.append(h(x[-1]))
+        x.append(actions[i](x[-1]))
         t.append(t[-1])
 
     return t, x
@@ -52,12 +62,12 @@ if __name__ == '__main__':
         IC = IP(t) - UC/R
         return IC/C
 
-    def g(t, UC):
+    def event(t, UC):
         IC = abs(IP(t)) - abs(UC)/R
         return IC
 
-    def h(UC):
+    def action(UC):
         return 0.0
 
-    t, x = odeint(f, g, h,
+    t, x = odeint(f, [event], [action],
         x0=0.0, t0=0.0, t1=3*math.pi, dt=0.001, eps=1e-15)
