7bd15b12421409ca04d7c101463d4e7218feb10a
Friedrich Hagedorn <fhgd@users.noreply.github.com>
2010-08-25 16:58:11 +0200

    Fast ideale Leistungsanpassung. Mit zeitdiskretem Zustand y.

--- hybridsim.py
+++ hybridsim.py
@@ -8,12 +8,13 @@ def fsolve(g, x0, x1, eps):
         x0, x1 = x1, x1 - g(x1) * (x1 - x0) / (g(x1) - g(x0))
     return x1
 
-def odeint(f, x0, t0, t1, dt, graph, z0, eps):
+def odeint(f, x0, t0, t1, dt, graph, z0, eps, y0):
     """Integration eines hybriden Systems (erster Ordnung)"""
 
     # start values
     t = [t0]
     x = [x0]
+    y = [y0]
     z = z0
 
     while t[-1] < t1:
@@ -23,6 +24,8 @@ def odeint(f, x0, t0, t1, dt, graph, z0, eps):
             # integrate with an euler step
             x.append(x[-1] + f(t[-1], x[-1]) * dt)
             t.append(t[-1] + dt)
+            # constant extention of the time discrete vaules
+            y.append(y[-1])
             # test all event functions
             events = [ev for ev in graph[z].iterkeys() if ev(t[-1], x[-1]) < 0]
             if events:
@@ -45,15 +48,18 @@ def odeint(f, x0, t0, t1, dt, graph, z0, eps):
 
         # transition and action of the FSM
         z = graph[z][event]
-        x.append(z(t[-1], x[-1]))
+        x_new, y_new = z(t[-1], x[-1], y[-1])
+        x.append(x_new)
+        y.append(y_new)
         t.append(t[-1])
-    return t, x
+    return t, x, y
 
 
 if __name__ == '__main__':
 
     R = 0.5
     C = 1.0
+    deltaUC = 0.02
 
     import math
 
@@ -64,12 +70,18 @@ if __name__ == '__main__':
         IC = IP(t) - UC/R
         return IC/C
 
-    def event(t, UC):
-        IC = abs(IP(t)) - abs(UC)/R
-        return IC
+    def HARVEST(t, UC, E):
+        UC_ref = R/2. * IP(t)
+        E_new = E + C/2*(UC**2 - UC_ref**2)
+        return UC_ref, E_new
 
-    def action(t, UC):
-        return 0.0
+    def ev_too_low(t, UC):
+        UC_ref = R/2. * IP(t)
+        return UC - (UC_ref - deltaUC)
 
-    graph = {action : {event : action}}
-    t, x = odeint(f, 0.0, 0.0, 3*math.pi, 0.001, graph, action, 1e-15)
+    def ev_too_high(t, UC):
+        UC_ref = R/2. * IP(t)
+        return (UC_ref + deltaUC) - UC
+
+    graph = {HARVEST : {ev_too_low : HARVEST, ev_too_high : HARVEST}}
+    t, x, y = odeint(f, 0.0, 0.0, 3*math.pi, 0.001, graph, HARVEST, 1e-15, 0.0)
